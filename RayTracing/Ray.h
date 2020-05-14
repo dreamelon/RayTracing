ea4e3191a39fc9021f16089eeb4cd8af1497a631
treadstone <shihao7679@qq.com>
2020-05-14 09:31:15 +0800

    增加ray

--- RayTracing/Ray.h
+++ RayTracing/Ray.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Vector.h"
+
+class Ray {
+public:
+		Ray(){}
+		Ray(const Vector3f& A, const Vector3f &B) : orig(A), dir(B) {}
+		Vector3f Orig() { return orig; }
+		Vector3f Dir() { return dir; }
+		Vector3f Dest(float t) { return orig + dir * t; }
+private:
+	Vector3f orig;
+	Vector3f dir;
+};
