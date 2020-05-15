1155f53484ce67d616fa3401cc1ba97b0539e119
treadstone <shihao7679@qq.com>
2020-05-16 02:27:45 +0800

    修改ray

--- RayTracing/Ray.h
+++ RayTracing/Ray.h
@@ -6,7 +6,8 @@ public:
 		Ray(){}
 		Ray(const Vector3f& A, const Vector3f &B) : orig(A), dir(B) {}
 		Vector3f Orig() { return orig; }
-		Vector3f Dir() { return dir; }
+	    Vector3f Dir() { return dir; }
+		const Vector3f Dir() const { return dir; }
 		Vector3f Dest(float t) { return orig + dir * t; }
 private:
 	Vector3f orig;
