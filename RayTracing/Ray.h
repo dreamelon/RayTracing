ea047e137008c53fc9ecd93e3c00eab90d115024
treadstone <shihao7679@qq.com>
2020-05-18 00:57:05 +0800

    与球相交

--- RayTracing/Ray.h
+++ RayTracing/Ray.h
@@ -6,9 +6,11 @@ public:
 		Ray(){}
 		Ray(const Vector3f& A, const Vector3f &B) : orig(A), dir(B) {}
 		Vector3f Orig() { return orig; }
+		const Vector3f& Orig() const { return orig; }
 	    Vector3f Dir() { return dir; }
-		const Vector3f Dir() const { return dir; }
+		const Vector3f& Dir() const { return dir; }
 		Vector3f Dest(float t) { return orig + dir * t; }
+		const Vector3f Dest(float t) const { return orig + dir * t; }
 private:
 	Vector3f orig;
 	Vector3f dir;
