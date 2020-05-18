9ceb88b9635fc3f90143b89965ec03ede21811e5
SNDA\wangshihao <shihao7679@qq.com>
2020-05-18 19:04:29 +0800

    添加物体类

--- RayTracing/Object.h
+++ RayTracing/Object.h
@@ -1,1 +1,6 @@
 #pragma once
+#include "Vector.h"
+#include "Ray.h"
+class Object {
+	virtual bool intersect(const Ray&, float, uint32_t, Vector2f&) const = 0;
+};
---
+++ RayTracing/Sphere.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "Object.h"
+class Sphere :public Object {
+
+};
