e5e431adcff3dea5754a740e100d75cc67cd8d64
treadstone <shihao7679@qq.com>
2020-05-25 09:25:49 +0800

    可相交的物体

---
+++ RayTracing/HitableList.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "HitableList.h"
+
+bool HitableList::intersect(const Ray& ray, float tmin, float tmax, Hit_record& rec) const
+{
+	Hit_record temp_rec;
+	bool hit_anything = false;
+	double closest_sofar = tmax;
+	for (int i = 0; i < list_size; i++) {
+		if (list[i]->intersect(ray, tmin, closest_sofar, temp_rec)) {
+			hit_anything = true;
+			closest_sofar = temp_rec.t;
+			rec = temp_rec;
+		}
+	}
+	return hit_anything;
+}
