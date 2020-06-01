11acfbbf2fa1e76434832891ce166935ef566255
treadstone <shihao7679@qq.com>
2020-06-01 20:16:05 +0800

    材质

---
+++ RayTracing/Material.h
@@ -0,0 +1,7 @@
+//#pragma once
+//#include "Ray.h"
+//#include "Object.h"
+//class Material {
+//public :
+//	virtual bool scatter(const Ray& in, const Hit_record& rec, color& attenuation, Ray& scattered) const = 0;
+//};
